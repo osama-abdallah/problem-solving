889f56e54a9f12abaab949fe34c2f2681324782e
osama-abdallah <osama.i.abdallah95@gmail.com>
2022-01-25 15:23:31 +0200

    commplete

--- Challenge06/Challenge06.js
+++ Challenge06/Challenge06.js
@@ -31,7 +31,7 @@ const findMax = (arr)=>{
 
 const sumNums = (arr)=>{
     let sum = 0; 
-    for (let i = 0; i<arr.length ; i++){
+    for (let i = 0; i<arr.length ; i++) {
 
             sum =+ arr[i];
     
